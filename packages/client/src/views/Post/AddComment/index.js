fefe34e18a2bcda382e077045b8aa8c594c6cd1e
Ariful islam <arifulbgt4@gmail.com>
2021-02-09 02:55:02 +0600

    feat: coment box authenticated

--- packages/client/src/views/Post/AddComment/index.js
+++ packages/client/src/views/Post/AddComment/index.js
@@ -1,29 +1,33 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const AddComment = () => {
+  const isAuthenticated = useSelector((store) => store.user.isAuthenticated);
   return (
-    <div className="add-comment">
-      <header>
-        <h3 className="h6">Leave a reply</h3>
-      </header>
-      <form action="#" className="commenting-form">
-        <div className="row">
-          <div className="form-group col-md-12">
-            <textarea
-              name="usercomment"
-              id="usercomment"
-              placeholder="Type your comment"
-              className="form-control"
-            ></textarea>
+    isAuthenticated && (
+      <div className='add-comment'>
+        <header>
+          <h3 className='h6'>Leave a reply</h3>
+        </header>
+        <form action='#' className='commenting-form'>
+          <div className='row'>
+            <div className='form-group col-md-12'>
+              <textarea
+                name='usercomment'
+                id='usercomment'
+                placeholder='Type your comment'
+                className='form-control'
+              ></textarea>
+            </div>
+            <div className='form-group col-md-12'>
+              <button type='submit' className='btn btn-secondary'>
+                Submit Comment
+              </button>
+            </div>
           </div>
-          <div className="form-group col-md-12">
-            <button type="submit" className="btn btn-secondary">
-              Submit Comment
-            </button>
-          </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+    )
   );
 };
 
