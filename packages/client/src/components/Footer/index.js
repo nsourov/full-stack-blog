8f9764edfd1d4fd9b544470a9a87b114fa3b99b8
Ariful islam <arifulbgt4@gmail.com>
2021-02-23 22:11:13 +0600

    fix: footer copyright

--- packages/client/src/components/Footer/index.js
+++ packages/client/src/components/Footer/index.js
@@ -1,56 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="main-footer">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 text-center">
-            <div className="logo">
-              <h6 className="text-white">Bootstrap Blog</h6>
+    <footer className='main-footer'>
+      <div className='copyrights'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-6 col-sm-12 text-sm-center'>
+              <p>&copy; 2021. All rights reserved.</p>
             </div>
-            <div className="contact-details">
-              <p>53 Broadway, Broklyn, NY 11249</p>
-              <p>Phone: (020) 123 456 789</p>
-              <p>
-                Email: <a href="mailto:info@company.com">Info@Company.com</a>
-              </p>
-              <ul className="social-menu">
-                <li className="list-inline-item">
-                  <a href="#">
-                    <i className="fa fa-facebook"></i>
-                  </a>
+            <div className='col-md-6 col-sm-12 text-sm-center text-md-right'>
+              <ul className='footer-nav'>
+                <li>
+                  <Link to='#'>Privacy</Link>
                 </li>
-                <li className="list-inline-item">
-                  <a href="#">
-                    <i className="fa fa-twitter"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#">
-                    <i className="fa fa-instagram"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#">
-                    <i className="fa fa-behance"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#">
-                    <i className="fa fa-pinterest"></i>
-                  </a>
+                <li>
+                  <Link to='#'>Terms of Use</Link>
                 </li>
               </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="copyrights">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12 text-center">
-              <p>&copy; 2021. All rights reserved. Your great site.</p>
             </div>
           </div>
         </div>
