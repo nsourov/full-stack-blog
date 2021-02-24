9a17aebb6042fa33758305a1ada094e7e652ab52
Ariful islam <arifulbgt4@gmail.com>
2021-02-24 07:53:10 +0600

    fix: typo 404 page

--- packages/client/src/components/404/index.js
+++ packages/client/src/components/404/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 page for found</h1>
+    <div className='col-12 py-5'>
+      <div className='d-flex justify-content-center'>
+        <h1>404 page not found</h1>
+      </div>
     </div>
   );
 };
