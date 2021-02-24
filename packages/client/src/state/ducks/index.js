34420241a285244688b9f8253f0474ea433c15d8
Ariful islam <arifulbgt4@gmail.com>
2021-02-24 12:07:40 +0600

    chore(clean): frontend client images

--- packages/client/src/state/ducks/index.js
+++ packages/client/src/state/ducks/index.js
@@ -2,15 +2,10 @@ import { combineReducers } from '@reduxjs/toolkit';
 
 import blogs from './blogs';
 import postCategories from './postCategories';
-import postComment from './postComment';
-
-import ui from './ui';
 import authentication from './authentication';
 
 export default combineReducers({
   ...blogs,
   ...postCategories,
-  ...postComment,
-  ...ui,
   ...authentication,
 });
